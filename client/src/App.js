0bb7677c3d3e845012b060e732753c146f4f6af5
Akash Bhave <akashbbhave@gmail.com>
2019-10-16 19:54:57 -0400

    Install Leaflet, create Map component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Map from './Map';
+
 function App() {
     return (
         <div className="App">
             <p>Test</p>
+            <Map />
         </div>
     );
 }
