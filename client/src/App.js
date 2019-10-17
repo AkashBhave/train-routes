86af1eec974c5e00ec057fffc1fdba98073a6533
Akash Bhave <akashbbhave@gmail.com>
2019-10-16 21:33:44 -0400

    Animate the train route with Leaflet

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 import Map from './components/Map';
 
-function App() {
-    return (
-        <div className="App">
-            <Map />
-        </div>
-    );
-}
+const App = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/solve?algorithm=a&city1=Leon&city2=Montreal').then(res => setData(res.data));
+    }, []);
+
+    return <div className="App">{data ? <Map points={data.solution} /> : <div>Loading...</div>}</div>;
+};
 
 export default App;
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,22 +1,56 @@
-import React, { Component } from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import React, { useRef, useState, useEffect } from 'react';
+import { Map, TileLayer, Polyline } from 'react-leaflet';
 
-export default class M extends Component {
-    state = {
-        lat: 39.315701,
-        lng: -99.636657,
-        zoom: 4
-    };
+// "Custom" callback to allow `setInterval` with state
+// Adapted from https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+const useInterval = (callback, delay) => {
+    const savedCallback = useRef();
 
-    render() {
-        const position = [this.state.lat, this.state.lng];
-        return (
-            <Map center={position} zoom={this.state.zoom}>
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
+};
+
+const M = ({ points }) => {
+    const [lat, setLat] = useState(39.315701);
+    const [lon, setLon] = useState(-99.636657);
+    const [zoom, setZoom] = useState(4);
+
+    const [pointsI, setPointsI] = useState(0);
+
+    // Keep updating the end index while index is below the total length
+    useInterval(
+        () => {
+            setPointsI(pointsI + 100);
+        },
+        pointsI < points.length ? 1 : null
+    );
+
+    return (
+        <>
+            <Map center={[lat, lon]} zoom={zoom}>
                 <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                {points.slice(0, pointsI).map((point, i) => (
+                    <Polyline key={i} color="red" positions={point} />
+                ))}
             </Map>
-        );
-    }
-}
+        </>
+    );
+};
+
+export default M;
